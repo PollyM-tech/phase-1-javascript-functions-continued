be55f4bb41dce85b87d5bd42b735fc5db71a6e4f
Pauline Moraa <pmoraa431@gmail.com>
2025-03-06 17:47:26 +0300

    continued functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity= 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  console.log(saturdayFun()); 
+  console.log(saturdayFun("watch football")); 
+  
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+console.log(mondayWork())
+console.log(mondayWork("freelance"));
+  
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
